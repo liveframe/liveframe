4c6890ffd1b0577154b52bb286cfe9ee02297ac6
William Rodriguez <me@williamrodriguez.com>
2020-09-28 21:22:39 -0400

    - Created master LiveFrame class that instantiates video and audio classes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@
 const Video = require('./FrameCapture.js').default;
 const Audio = require('./AudioCapture.js').default;
 
-export { Video, Audio };
+class LiveFrame {
+    video = null;
+    audio = null;
+
+    constructor(config) {
+        this.#options = config.options;
+
+        if (this.#options.includes('webcam', 'screenshare')) {
+            this.video = new Video({
+                token: config.token,
+                interval: config.interval.video,
+                // width: config.constraints.width,
+                quality: config.constraints.quality
+            });
+        }
+
+        if (this.#options.includes('audio')) {
+            this.audio = new Audio({
+                token: config.token,
+                interval: config.interval.audio
+            });
+        }
+    }
+    start() {
+        if (this.video) this.video.start();
+        if (this.audio) this.audio.start();
+    }
+    stop() {
+        if (this.video) this.video.stop();
+        if (this.audio) this.audio.stop();
+    }
+}
+
+export default LiveFrame;
